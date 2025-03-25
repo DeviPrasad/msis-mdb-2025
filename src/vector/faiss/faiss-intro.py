b47ae3d1c8de4cf0ac0833f4477e2a0283a6373b
Devi Prasad <dp@web3pleb.org>
2025-03-25 10:41:20 +0530

    Outputs of a few runs added for discussion.

--- src/vector/faiss/faiss-intro.py
+++ src/vector/faiss/faiss-intro.py
@@ -19,17 +19,41 @@ import faiss
 
 
 def hnsw_populate(n):
-    M = 16
+    M = 8
     D = 2
     np.random.seed()
     index = faiss.IndexHNSWFlat(D, M)
     population = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     index.hnsw.reset()
+    print(f"max_level {index.hnsw.max_level}, index total {index.ntotal}")
     for i in range(n):
         l = index.hnsw.random_level()
         population[l] += 1
         index.add(np.random.random((1, D)).astype(np.float32))
     print(population)
+    print(f"max_level {index.hnsw.max_level}, index total {index.ntotal}")
 
 
-hnsw_populate(1000)
+hnsw_populate(1024 * 8)
+
+"""
+M = 8
+>>> hnsw_populate(1000 * 4)
+[7143, 915, 121, 12, 0, 1, 0, 0, 0, 0, 0]
+>>> hnsw_populate(1000 * 16)
+[15003, 948, 45, 4, 0, 0, 0, 0, 0, 0, 0]
+>>> hnsw_populate(1000 * 16)
+[15003, 948, 45, 4, 0, 0, 0, 0, 0, 0, 0]
+>>> hnsw_populate(1000 * 128)
+[119842, 7671, 459, 26, 1, 1, 0, 0, 0, 0, 0]
+>>> hnsw_populate(1000 * 128)
+[119842, 7671, 459, 26, 1, 1, 0, 0, 0, 0, 0]
+>>> hnsw_populate(1000 * 512)
+[479879, 30149, 1865, 98, 8, 1, 0, 0, 0, 0, 0]
+>>> hnsw_populate(1000 * 1024)
+[960030, 59972, 3780, 201, 16, 1, 0, 0, 0, 0, 0]
+>>> hnsw_populate(1000 * 1024 * 8)
+[7679750, 480171, 30050, 1922, 101, 5, 1, 0, 0, 0, 0]
+>>> hnsw_populate(1000 * 1024 * 128)
+[122880728, 7678938, 480436, 29918, 1859, 112, 8, 1, 0, 0, 0]
+"""
